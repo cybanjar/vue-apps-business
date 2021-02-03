35c04a104a198eab9be5c9185c5f6fe2a5a2c19a
irfan nurzaman <inurzmn@gmail.com>
2021-02-03 17:06:10 +0700

    SS

--- src/views/dashboard/NewPost.vue
+++ src/views/dashboard/NewPost.vue
@@ -4,37 +4,37 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 mx-auto">
-                    <form>
+                    <form @submit.prevent="post">
                         <div class="form-group">
                             <label for="nama">Nama</label>
-                            <input type="text" class="form-control" id="nama">
+                            <input type="text" class="form-control" id="nama" v-model="name">
                         </div>
                         <div class="form-group">
                             <label for="harga">Harga</label>
-                            <input type="number" class="form-control" id="harga">
+                            <input type="number" class="form-control" id="harga" v-model="harga">
                         </div>
                         <div class="form-group">
                             <label for="deskripsi">Deskripsi</label>
-                            <input type="text" class="form-control" id="deskripsi">
+                            <input type="text" class="form-control" id="deskripsi" v-model="deskripsi">
                         </div>
                         <div class="form-group">
                             <label for="kondisi">Kondisi</label>
-                            <select class="form-control" id="kondisi">
+                            <select class="form-control" id="kondisi" v-model="kondisi">
                                 <option>Baru</option>
                                 <option>Bekas</option>
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="lokasi">Lokasi</label>
-                            <input type="text" class="form-control" id="lokasi">
+                            <input type="text" class="form-control" id="lokasi" v-model="lokasi">
                         </div>
                         <div class="form-group">
                             <label for="kategori">Kategori</label>
-                            <input type="text" class="form-control" id="kategori">
+                            <input type="text" class="form-control" id="kategori" v-model="kategory">
                         </div>
                         <div class="form-group">
                             <label for="photo">Upload</label>
-                            <input type="file" class="form-control-file" id="photo">
+                            <input type="file" class="form-control-file" id="photo" @change="onFileChange">
                         </div>
                         <button type="submit" class="btn btn-primary float-right">Submit</button>
                     </form>
@@ -58,6 +58,13 @@ import axios from 'axios'
             const state = reactive({                
                 dataPost: [],
                 user: "",
+                name: '',
+                harga: '',
+                deskripsi: '',
+                kondisi: '',
+                lokasi: '',
+                kategory: '',
+                image: ''
             })
 
             //state token
@@ -114,10 +121,24 @@ import axios from 'axios'
 
             }
 
+            function post(){
+                const {name, harga, deskripsi, kondisi, lokasi, kategory, image} = state
+                console.log('sukses', name, harga, deskripsi, kondisi, lokasi, kategory, image);
+            }
+
+            function onFileChange(e) {
+                const files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                return;
+                state.image = files[0]
+            }
+
             return {
                 ...toRefs(state),
                 token,      // <-- state token
                 logout,     // <-- method logout
+                post,
+                onFileChange
 
             }
 
